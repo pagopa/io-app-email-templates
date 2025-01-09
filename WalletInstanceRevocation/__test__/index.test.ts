d8ea711766cfd5a97616eeaf4976a8c7895159b4
christianatzeni <christian.alessandro.atzeni@it.ey.com>
2025-01-09 11:29:21 +0100

    Fixing unit tests for WalletInstanceRevocation email template

--- WalletInstanceRevocation/__test__/index.test.ts
+++ WalletInstanceRevocation/__test__/index.test.ts
@@ -1,6 +1,5 @@
 import { describe, it, expect, vi, afterEach } from "vitest";
 import { ValidUrl } from "@pagopa/ts-commons/lib/url";
-import { FiscalCode } from "@pagopa/ts-commons/lib/strings";
 import { apply } from "../";
 
 describe("Snapshot testing", () => {
@@ -9,12 +8,9 @@ describe("Snapshot testing", () => {
   });
 
   it("should match snapshot", () => {
-    const htmlOutput: string = apply(
-      "LVTEST00A00A0001" as FiscalCode,
-      "10:00",
-      "20/12/2024",
-      { href: "https://test.test/" } as ValidUrl
-    );
+    const htmlOutput: string = apply("10:00", "20/12/2024", {
+      href: "https://test.test/",
+    } as ValidUrl);
 
     expect(htmlOutput).toMatchSnapshot();
   });
