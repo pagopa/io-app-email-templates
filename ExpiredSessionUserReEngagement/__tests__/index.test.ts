932ed673f23e1ac692116374935711f4af39a116
Salvatore Laiso <salvatore.laiso@it.ey.com>
2025-03-05 12:18:15 +0100

    feat: update email template with new text and expiration date

--- ExpiredSessionUserReEngagement/__tests__/index.test.ts
+++ ExpiredSessionUserReEngagement/__tests__/index.test.ts
@@ -5,7 +5,8 @@ import { ValidUrl } from "@pagopa/ts-commons/lib/url";
 describe("Snapshot testing", () => {
   it("should match snapshot", () => {
     const htmlOutput: string = applier.apply(
-      { href: "https://test.test/" } as ValidUrl
+      { href: "https://test.test/" } as ValidUrl,
+      new Date("2022-01-01T00:00:00Z")
     );
     expect(htmlOutput).toMatchSnapshot();
   });
