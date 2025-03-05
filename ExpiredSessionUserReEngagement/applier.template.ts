150275e879ab69d801a1b7d82627e8ccd1ddbef3
Salvatore Laiso <32564922+salvatorelaiso@users.noreply.github.com>
2025-03-05 15:10:40 +0100

    [#IOPID-2777] Update ExpiredSessionUserReEngagement with session expiration date
    Co-authored-by: Salvatore Laiso <salvatore.laiso@it.ey.com>

--- ExpiredSessionUserReEngagement/applier.template.ts
+++ ExpiredSessionUserReEngagement/applier.template.ts
@@ -1,8 +1,12 @@
 import { ValidUrl } from "@pagopa/ts-commons/lib/url";
+import { DateFromTimestamp } from "@pagopa/ts-commons/lib/dates";
+import { extractDate } from "../utils/datetime";
 
 export const apply = (
     ctaUrl: ValidUrl,
+    date: DateFromTimestamp,
 ): string => {
     const cta = ctaUrl.href
+    const expirationDate = extractDate(date);
     return `{{TEMPLATE}}`;
 };
