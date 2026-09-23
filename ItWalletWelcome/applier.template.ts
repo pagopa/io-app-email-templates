a7027a9c97b2299794af7cae9f17b08202c6ab5b
MonicaRungi-EY <Monica.Rungi@it.ey.com>
2026-09-23 11:32:24 +0200

    feat(IT Wallet): update welcome email content and structure

--- ItWalletWelcome/applier.template.ts
+++ ItWalletWelcome/applier.template.ts
@@ -1,9 +1,11 @@
 import { ValidUrl } from "@pagopa/ts-commons/lib/url";
 
 export const apply = (
+  helpCenterLinkURL: ValidUrl,
   disableItWalletLinkURL: ValidUrl,
-  ioWebAppLinkUrl: ValidUrl
+  ioWebAppLinkUrl: ValidUrl,
 ): string => {
+  const helpCenterLink = helpCenterLinkURL.href;
   const disableItWalletLink = disableItWalletLinkURL.href;
   const ioWebAppLink = ioWebAppLinkUrl.href;
   return `{{TEMPLATE}}`;
